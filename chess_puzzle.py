394f1ed332d4a44a2438adc476e525b0d928de1f
James Baker <bakerje@mac.com>
2022-05-16 14:17:26 +0100

    Continued with coursework

--- chess_puzzle.py
+++ chess_puzzle.py
@@ -1,11 +1,239 @@
-def read_board(filename):
+from typing import *
+def location2index(loc: str) -> Tuple[int, int]:
+    '''converts chess location to corresponding x and y coordinates'''
+    #  tuple called coords to hold co-ordinates in x,y format
+    coords = ()
+    # check the first coordinate is a letter from a - z
+    if loc[0].isalpha() == False:
+        return "The first coordinate must be a letter"
+    # Populate tuple with x, y coordinates.  Check that second coordinte is an integer no longer than the length of the board.
+    try:
+        int(loc[1:])
+        if int(loc[1:]) > 26:
+            raise ValueError
+            print("Invalid y value is larger than board size.")
+        coords = ((int(ord(loc[0].lower())) - 96), int(loc[1:]))
+    except Exception as e:
+        raise ValueError
+        print("Second item must be an integer: ", e)
+
+    return coords
+
+
+def index2location(x: int, y: int) -> str:
+    '''converts  pair of coordinates to corresponding location'''
+    # Check for invalid string coordinates and return ValueError
+    if type(x) != int or type(y) != int:
+        raise ValueError
+        print("x or y coordinate must be an integer")
+    if x > 26 or y > 26:
+        raise ValueError
+        print("x or y coordinate must be less than maximum board length")
+
+    str_coord = (chr(x + 96) + str(y))
+    print(str_coord)
+    return str_coord
+
+
+class Piece:
+    pos_x: int
+    pos_y: int
+    side: bool  # True for White and False for Black
+
+    def __init__(self, pos_X: int, pos_Y: int, side_: bool):
+        '''sets initial values'''
+        self.pos_X = pos_X
+        self.pos_y = pos_Y
+        self.side = bool
+
+
+Board = Tuple[int, List[Piece]]
+
+
+def is_piece_at(pos_X: int, pos_Y: int, B: Board) -> bool:
+    '''checks if there is piece at coordinates pox_X, pos_Y of board B'''
+
+
+def piece_at(pos_X: int, pos_Y: int, B: Board) -> Piece:
+    '''
+    returns the piece at coordinates pox_X, pos_Y of board B
+    assumes some piece at coordinates pox_X, pos_Y of board B is present
+    '''
+
+
+class Rook(Piece):
+    def __init__(self, pos_X: int, pos_Y: int, side_: bool):
+        '''sets initial values by calling the constructor of Piece'''
+
+    def can_reach(self, pos_X: int, pos_Y: int, B: Board) -> bool:
+        '''
+        checks if this rook can move to coordinates pos_X, pos_Y
+        on board B according to rule [Rule2] and [Rule4](see section Intro)
+        Hint: use is_piece_at
+        '''
+
+    def can_move_to(self, pos_X: int, pos_Y: int, B: Board) -> bool:
+        '''
+        checks if this rook can move to coordinates pos_X, pos_Y
+        on board B according to all chess rules
+
+        Hints:
+        - firstly, check [Rule2] and [Rule4] using can_reach
+        - secondly, check if result of move is capture using is_piece_at
+        - if yes, find the piece captured using piece_at
+        - thirdly, construct new board resulting from move
+        - finally, to check [Rule5], use is_check on new board
+        '''
+
+    def move_to(self, pos_X: int, pos_Y: int, B: Board) -> Board:
+        '''
+        returns new board resulting from move of this rook to coordinates pos_X, pos_Y on board B
+        assumes this move is valid according to chess rules
+        '''
+
+
+class Bishop(Piece):
+    def __init__(self, pos_X: int, pos_Y: int, side_: bool):
+        '''sets initial values by calling the constructor of Piece'''
+        Piece.__init__(self, pos_X, pos_Y, side_)
+
+    def can_reach(self, pos_X: int, pos_Y: int, B: Board) -> bool:
+        '''checks if this bishop can move to coordinates pos_X, pos_Y on board B according to rule [Rule1] and [Rule4]'''
+
+    def can_move_to(self, pos_X: int, pos_Y: int, B: Board) -> bool:
+        '''checks if this bishop can move to coordinates pos_X, pos_Y on board B according to all chess rules'''
+
+    def move_to(self, pos_X: int, pos_Y: int, B: Board) -> Board:
+        '''
+        returns new board resulting from move of this bishop to coordinates pos_X, pos_Y on board B
+        assumes this move is valid according to chess rules
+        '''
+
+
+class King(Piece):
+    def __init__(self, pos_X: int, pos_Y: int, side_: bool):
+        '''sets initial values by calling the constructor of Piece'''
+
+    def can_reach(self, pos_X: int, pos_Y: int, B: Board) -> bool:
+        '''checks if this king can move to coordinates pos_X, pos_Y on board B according to rule [Rule3] and [Rule4]'''
+        # return abs(col_from - col_to)+abs(row_from - row_to) == 3
+
+    def can_move_to(self, pos_X: int, pos_Y: int, B: Board) -> bool:
+        '''checks if this king can move to coordinates pos_X, pos_Y on board B according to all chess rules'''
+
+    def move_to(self, pos_X: int, pos_Y: int, B: Board) -> Board:
+        '''
+        returns new board resulting from move of this king to coordinates pos_X, pos_Y on board B
+        assumes this move is valid according to chess rules
+        '''
+
+
+def is_check(side: bool, B: Board) -> bool:
+    '''
+    checks if configuration of B is check for side
+    Hint: use can_reach
+    '''
+
+
+def is_checkmate(side: bool, B: Board) -> bool:
+    '''
+    checks if configuration of B is checkmate for side
+
+    Hints:
+    - use is_check
+    - use can_reach
+    '''
+
+
+def is_stalemate(side: bool, B: Board) -> bool:
+    '''
+    checks if configuration of B is stalemate for side
+
+    Hints:
+    - use is_check
+    - use can_move_to
+    '''
+
+
+def read_board(filename: str) -> Board:
     '''
     reads board configuration from file in current directory in plain format
     raises IOError exception if file is not valid (see section Plain board configurations)
     '''
+    Board1 = tuple()
     Board_arr = []
     board = []
-    infile = open(filename, "r")
-    while True:
+    run = True
+    while run == True:
+        try:
+            infile = open(filename, "r")
+            lines = infile.readlines()
+            if len(lines) != 3:
+                infile.close()
+                filename = input("This is not a valid file. File name for initial configuration: ")
+        except IOError:
+            filename = input("This is not a valid file. File name for initial configuration: ")
+        else:
+            infile = open(filename, "r")
+            line = infile.readline()
+            while line != "":
+                board.append(line.rstrip())
+                line = infile.readline()
+            Board_arr = [int(board[0]), board[1].split(","), board[2].split(",")]
+            run = False
 
-read_board("board_examp.txt")
+    Board1 += (Board_arr[0], )
+    pieces_arr = []
+    count_B = 0
+    for i in range(1, 3):
+        if i == 1:
+            for j in range(0, len(Board_arr[i])):
+                Board_arr[i][j] = Board_arr[i][j].strip()
+                xy_loc = location2index(Board_arr[i][j][1:])
+                if Board_arr[i][j][0] == "B":
+                    count_B += 1
+                    name = "wb" + str(count_B)
+                    print(name)
+                    name = Bishop(xy_loc[0], xy_loc[1], True)
+                    pieces_arr.append(name)
+            Board1 += (pieces_arr,)
+    print(Board_arr)
+    print(pieces_arr)
+    print(Board1)
+    return Board1
+
+
+def save_board(filename: str, B: Board) -> None:
+    '''saves board configuration into file in current directory in plain format'''
+
+
+def find_black_move(B: Board) -> Tuple[Piece, int, int]:
+    '''
+    returns (P, x, y) where a Black piece P can move on B to coordinates x,y according to chess rules
+    assumes there is at least one black piece that can move somewhere
+
+    Hints:
+    - use methods of random library
+    - use can_move_to
+    '''
+
+
+def conf2unicode(B: Board) -> str:
+    '''converts board cofiguration B to unicode format string (see section Unicode board configurations)'''
+
+
+def main() -> None:
+    '''
+    runs the play
+
+    Hint: implementation of this could start as follows:
+    filename = input("File name for initial configuration: ")
+    ...
+    '''
+
+    #filename = input("File name for initial configuration: ")
+    read_board("board_examp.txt")
+
+
+if __name__ == '__main__':  # keep this in
+    main()
---
+++ test_chess_puzzle.py
@@ -0,0 +1,116 @@
+import pytest
+from typing import *
+from chess_puzzle import *
+
+
+def test_locatio2index1():
+    assert location2index("e2") == (5, 2)
+
+    assert location2index("S8") == (19, 8)
+
+    assert location2index("52") == "The first coordinate must be a letter"
+
+    with pytest.raises(ValueError):
+        location2index("aa")
+
+    with pytest.raises(ValueError):
+        location2index("a123")
+
+
+def test_index2location1():
+    assert index2location(5, 2) == "e2"
+
+    assert index2location(10, 14) == "j14"
+
+    assert index2location(23, 2) == "w2"
+
+    with pytest.raises(ValueError):
+        index2location("e", 5)
+
+    with pytest.raises(ValueError):
+        index2location(3, "d")
+
+
+wb1 = Bishop(1, 1, True)
+wr1 = Rook(1, 2, True)
+wb2 = Bishop(5, 2, True)
+bk = King(2, 3, False)
+br1 = Rook(4, 3, False)
+br2 = Rook(2, 4, False)
+br3 = Rook(5, 4, False)
+wr2 = Rook(1, 5, True)
+wk = King(3, 5, True)
+
+B1 = (5, [wb1, wr1, wb2, bk, br1, br2, br3, wr2, wk])
+'''
+♖ ♔  
+ ♜  ♜
+ ♚ ♜ 
+♖   ♗
+♗    
+'''
+
+
+def test_is_piece_at1():
+    assert is_piece_at(2, 2, B1) == False
+
+
+def test_piece_at1():
+    assert piece_at(4, 3, B1) == br1
+
+
+def test_can_reach1():
+    assert wr2.can_reach(4, 5, B1) == False
+
+
+br2a = Rook(1, 5, False)
+wr2a = Rook(2, 5, True)
+
+
+def test_can_move_to1():
+    B2 = (5, [wb1, wr1, wb2, bk, br1, br2a, br3, wr2a, wk])
+    assert wr2a.can_move_to(2, 4, B2) == False
+
+
+def test_is_check1():
+    wr2b = Rook(2, 4, True)
+    B2 = (5, [wb1, wr1, wb2, bk, br1, br2a, br3, wr2b, wk])
+    assert is_check(True, B2) == True
+
+
+def test_is_checkmate1():
+    br2b = Rook(4, 5, False)
+    B2 = (5, [wb1, wr1, wb2, bk, br1, br2b, br3, wr2, wk])
+    assert is_checkmate(True, B2) == True
+
+
+def test_read_board1():
+    with pytest.raises(IOError):
+        read_board("d")
+
+    with pytest.raises(IOError):
+        read_board("board_examp1.txt")
+
+    B = read_board("board_examp.txt")
+    assert B[0] == 5
+
+    for piece in B[1]:  # we check if every piece in B is also present in B1; if not, the test will fail
+        found = False
+        for piece1 in B1[1]:
+            if piece.pos_x == piece1.pos_x and piece.pos_y == piece1.pos_y and piece.side == piece1.side and type(
+                    piece) == type(piece1):
+                found = True
+        assert found
+
+    for piece1 in B1[1]:  # we check if every piece in B1 is also present in B; if not, the test will fail
+        found = False
+        for piece in B[1]:
+            if piece.pos_x == piece1.pos_x and piece.pos_y == piece1.pos_y and piece.side == piece1.side and type(
+                    piece) == type(piece1):
+                found = True
+        assert found
+
+
+def test_conf2unicode1():
+    assert conf2unicode(B1) == "♖ ♔  \n ♜  ♜\n ♚ ♜ \n♖   ♗\n♗    "
+
